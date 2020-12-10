632da429d8fc30f794c1a70ea91fe71959700838
FloBonita <74161621+FloBonita@users.noreply.github.com>
2020-12-10 17:02:42 -0300

    Utilizando loopins em C#

---
+++ 13_10/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace _13_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnDoWhile = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.BackColor = System.Drawing.Color.White;
+            this.lstNumbers.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumbers.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 25;
+            this.lstNumbers.Location = new System.Drawing.Point(13, 35);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(150, 304);
+            this.lstNumbers.TabIndex = 0;
+            // 
+            // btnFor
+            // 
+            this.btnFor.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnFor.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFor.Location = new System.Drawing.Point(214, 35);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(126, 33);
+            this.btnFor.TabIndex = 1;
+            this.btnFor.Text = "For";
+            this.btnFor.UseVisualStyleBackColor = false;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnWhile.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWhile.Location = new System.Drawing.Point(214, 93);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(126, 33);
+            this.btnWhile.TabIndex = 2;
+            this.btnWhile.Text = "While";
+            this.btnWhile.UseVisualStyleBackColor = false;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // btnDoWhile
+            // 
+            this.btnDoWhile.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnDoWhile.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoWhile.Location = new System.Drawing.Point(214, 154);
+            this.btnDoWhile.Name = "btnDoWhile";
+            this.btnDoWhile.Size = new System.Drawing.Size(126, 33);
+            this.btnDoWhile.TabIndex = 3;
+            this.btnDoWhile.Text = "Do While";
+            this.btnDoWhile.UseVisualStyleBackColor = false;
+            this.btnDoWhile.Click += new System.EventHandler(this.btnDoWhile_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnClear.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(214, 221);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(126, 33);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clean";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(444, 393);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDoWhile);
+            this.Controls.Add(this.btnWhile);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.lstNumbers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnDoWhile;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ 13_10/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            
+           lstNumbers.Items.Clear();
+          
+            int i;
+            for (i = 0; i <= 100; i++)
+             {
+              lstNumbers.Items.Add(i);
+             }
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            lstNumbers.Items.Clear();
+            int i = 1;
+            while (i <= 100)
+            {
+            lstNumbers.Items.Add(i);
+            i++;
+            }
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+            lstNumbers.Items.Clear();
+            int i = 1;
+            do
+            {
+             lstNumbers.Items.Add(i);
+             i++;
+             } while (i <= 100) ;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstNumbers.Items.Clear();
+        }
+    }
+}
